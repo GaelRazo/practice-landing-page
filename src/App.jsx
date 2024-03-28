bc038e66cb61676274b5c633ce6b7a712247ff35
GaelRazo <57119167+GaelRazo@users.noreply.github.com>
2024-03-28 16:53:21 -0600

    Router Dom
    instalacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import { Hero, Navbar,Companies, Courses, Achievement, Categories, Feedback, CTA, Footer } from './components';
 import './App.css'
+import { BrowserRouter as Router, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div>
-      <Navbar />
-      <Hero />
-      <Companies/>
-      <Courses />
-      <Achievement />
-      <Categories />
-      <Feedback />
-      <CTA />
-      <Footer />
-    </div>
+    <Router>
+      <Routes></Routes>
+      <div>
+        <Navbar />
+        <Hero />
+        <Companies/>
+        <Courses />
+        <Achievement />
+        <Categories />
+        <Feedback />
+        <CTA />
+        <Footer />
+      </div>
+    </Router>
+    
   )
 }
 
