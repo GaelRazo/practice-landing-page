106dde8a3b90ce7d23d0f0fb6a25529b0ef22f9e
GaelRazo <57119167+GaelRazo@users.noreply.github.com>
2024-04-19 19:08:20 -0600

    Facebook Content

--- src/data/Courses.js
+++ src/data/Courses.js
@@ -1,75 +1,75 @@
 export const courses = [
     {
       id: 1,
-      title: 'The Complete Flutter Development Bootcamp with Dart',
-      category:'Flutter',
+      title: 'bisagras de alacenas',
+      category:'Mantenimiento',
       rating:5,
-      price: '$59.99',
+      price: 'Mantenimientoa bisagras de alacenas, cajoneras, quemadores de estufa, quemador de horno, campana extractora, en calle Roble 162 en Fracc. El pirul en Salamanca ,GTO.',
       linkImg:
-      'https://imgs.search.brave.com/YrWdDcc30vLk1ujOOb2T7YspGn9p6AsTC4LbTGaWiJI/rs:fit:480:270:1/g:ce/aHR0cHM6Ly93ZWVr/Y291cnNlcmV2aWV3/LmNvbS93cC1jb250/ZW50L3VwbG9hZHMv/MjAxOS8wNS9mbHV0/dGVyLWJvb3RjYW1w/LXdpdGgtZGFydC1p/bWcuanBn'
+      'https://scontent.fcyw3-1.fna.fbcdn.net/v/t39.30808-6/430842177_781827480634638_5674860463362066538_n.jpg?stp=dst-jpg_p180x540&_nc_cat=102&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeEU9moROGNPW6QUpOOt2giQKhS4vhAlP0QqFLi-ECU_RJMbOkB1mfx82MiLiohqVjV679wSUuk0xS8wOf1kWcML&_nc_ohc=unhqhKQal3gAb4fJmXc&_nc_ht=scontent.fcyw3-1.fna&oh=00_AfBEksE9JbVsS8-7J3gA5SuxHh-Nsh7gQd14ja0wSEjxJA&oe=6628E1A3'
     },
     {
       id: 2,
-      title: 'The Complete 2023 Web Development Bootcamp',
-      category:'Web Dev',
+      title: 'Claro de 9.00 x 6:00mts',
+      category:'Instalacion',
       rating:4,
-      price: '$59.99',
+      price: 'Trabajo aplicación de fondo estructural y pintura color negro brillante ha terminado con claro de 9.00 x 6:00mts calle Malvas 154 en Fracc. Arboledas II en Salamanca,GTO.',
       linkImg:
-        'https://imgs.search.brave.com/ZkwGbElTOq8Zo7NeRU669-o016O6XSLJftZZ5I--HJg/rs:fit:750:422:1/g:ce/aHR0cHM6Ly9kaXNj/b3VudHNlcmllcy5j/b20vd3AtY29udGVu/dC91cGxvYWRzLzIw/MjAvMTEvMTAwLWRh/eXMtb2YtY29kZS10/aGUtY29tcGxldGUt/cHl0aG9uLXByby1i/b290Y2FtcC1mb3It/MjAyMi1jb3Vyc2Uu/anBn',
+        'https://scontent.fcyw3-1.fna.fbcdn.net/v/t39.30808-6/423248756_760773976073322_1155984498216966714_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeFjhlh8h-Ff9fXvg0mrTzOplXg6XyPPGYuVeDpfI88Zi68g3kxm2CYj81pHgVqGjeoKxOUOdstwzLUP27Jn2p31&_nc_ohc=Su58-Phe1z4Ab5-1MNQ&_nc_ht=scontent.fcyw3-1.fna&oh=00_AfDQVNu0_mZvcEavzjvJdQnX8AS1QXm9UeeY_wMeONCyWw&oe=6628F2C0',
     },
     {
       id: 3,
-      title: 'Learn Python: The Complete Python Programming CourseLearn A-Z everything about Python, from the basics, to advanced topics like Python GUI, Python Data Analysis, and more!',
-      category:'Python',
+      title: 'Ajuste de máquina',
+      category:'Mecanica',
       rating:4,
-      price: '$59.99',
+      price: 'Ajuste de máquina de Cruze 2011, cambio de anillos de pistón, ajuste de cabeza, reemplazo de sellos, retenes, empaques en calle lagunitas 138 en Fracc Perul en Salamanca,gto',
       linkImg:
-        'https://imgs.search.brave.com/Nhj_cLAZOWcUjlnR8QMX8A02rX-2RYra9w-0j-B9Rbg/rs:fit:750:422:1/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzL2IxLzJi/L2NjL2IxMmJjYzA4/YmQ4YzlhOTFjMmM5/NjY3ZTM5NzE1NzA0/LmpwZw',
+        'https://scontent.fcyw3-1.fna.fbcdn.net/v/t39.30808-6/378222784_680223127461741_8431786887935482935_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeEJcy--mXoqra3zQzakiuP38mMRgaEtnYzyYxGBoS2djGKhggqdeYsiVeZAYUccPasdhi2vbH6B98SvoBYWPfV_&_nc_ohc=5tjO4xj4xtkAb6-I76r&_nc_ht=scontent.fcyw3-1.fna&oh=00_AfBbGZsXF1alIVfcWHAe_6i_rqF186GYzN0NP9Fk5kIbxQ&oe=6628E183',
     },
     {
       id: 4,
-      title: 'The Complete JavaScript Course 2023: From Zero to Expert!',
-      category:'Javascript',
+      title: 'Jardineria en cesped, follaje y cetos',
+      category:'Jardineria',
       rating:4,
-      price: '$59.99',
+      price: 'Mantenimiento preventivo de jardineria en cesped, follaje y cetos de casa habitación ubicada en la calle tecolutla numero 111 en Bellavista en Salamanca, GTO.',
       linkImg:
-        'https://imgs.search.brave.com/ERyn_YUVTbB0slaulPioLMmU7Zto9A8aCA6OPptRZkI/rs:fit:768:432:1/g:ce/aHR0cHM6Ly9jb3Vy/c2Vjb3Vwb25jbHVi/LmNvbS93cC1jb250/ZW50L3VwbG9hZHMv/MjAyMS8wMy84NTE3/MTJfZmM2MV82LTc2/OHg0MzIuanBn',
+        'https://scontent.fcyw3-1.fna.fbcdn.net/v/t39.30808-6/366819823_663998022417585_2038304190882771006_n.jpg?stp=dst-jpg_s600x600&_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeGpD9c9Ido3vH_QlZ8EsFeQX0M0r4BoY-BfQzSvgGhj4Fy6VQDrTJLdefiB50bxs8VhHrAoqmL-e9PquURV8pUE&_nc_ohc=DS69mIcUiP0Ab47TrTt&_nc_ht=scontent.fcyw3-1.fna&oh=00_AfBDWqSGEaar311xmWh0I_W2GJx-f6fdTW_evU4zDHH8hw&oe=6628F27A',
     },
     {
       id: 5,
-      title: 'Cuphead',
-      category:'Flutter',
+      title: 'Pintura y resanamiento',
+      category:'Pintura',
       rating:4,
-      price: '$19.99',
+      price: 'Comienzan los trabajos de pintura y resanamiento de interiores a dos manos en muros en casa habitación en Ahora 150 Fracc Galerias en Salamanca, GTO.',
       linkImg:
-        'https://assets.nintendo.com/image/upload/ar_16:9,b_auto:border,c_lpad/b_white/f_auto/q_auto/dpr_auto/c_scale,w_300/v1/ncom/en_US/games/switch/c/cuphead-switch/hero?_a=AJADJWI0',
+        'https://scontent.fcyw3-1.fna.fbcdn.net/v/t39.30808-6/349151243_576992377948629_1756132249476407402_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeEsdCdoa85riH2-DB7pDEfXI5dIL3a3qgUjl0gvdreqBfRU38OtTCJZYuklgNvY9Zw6iP7f_JkqCpqQmvAobOuQ&_nc_ohc=YSoZtY0nUcoAb62zIJT&_nc_ht=scontent.fcyw3-1.fna&oh=00_AfB4q8JR1X-21FwX80S_b3eAQ_04PYiU0Fm9uAe4Eh9obA&oe=6628E8EA',
     },
     {
       id: 6,
-      title: 'Minecraft',
-      category:'Flutter',
+      title: 'Mantenimiento y pintura en departamento',
+      category:'Mantenimiento',
       rating:4,
-      price: '$29.99',
+      price: 'Mantenimiento y pintura en departamento, pulimento de pisos, instalación de flotadores en tanques sanitarios, colocación de cortineros,calle Hidalgo en Salamanca,gto.',
       linkImg:
-        'https://assets.nintendo.com/image/upload/ar_16:9,b_auto:border,c_lpad/b_white/f_auto/q_auto/dpr_auto/c_scale,w_300/v1/ncom/en_US/games/switch/m/minecraft-switch/hero?_a=AJADJWI0',
+        'https://scontent.fcyw3-1.fna.fbcdn.net/v/t39.30808-6/343128658_1190130245712785_6233581807658445975_n.jpg?stp=dst-jpg_s600x600&_nc_cat=103&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeF4FxTuO5hh83aTPp6S5DLTws9mJ63gbqTCz2YnreBupJHQa56UGwfu1ihRNbY4SyjfwJjgiwod7wXi0Mq68Zq8&_nc_ohc=Y9Am0xHD9egAb4MxEPl&_nc_ht=scontent.fcyw3-1.fna&oh=00_AfCd96lXEmKBIOG-WEo5hs7eW3tl1dARRLv5yKdNIfiJmA&oe=6628DB28',
     },
     {
       id: 7,
-      title: 'Mario + Rabbids® Kingdom Battle',
-      category:'Flutter',
+      title: 'Reforestación y jardineria estetica',
+      category:'Jardineria',
       rating:4,
-      price: '$59.99',
+      price: 'Comienzan los trabajos de reforestación y jardineria estetica de cactus en casa habitación ubicada en la calle Tecolutla, Bellavista en Salamanca,gto. Higuar ',
       linkImg:
-        'https://assets.nintendo.com/image/upload/ar_16:9,b_auto:border,c_lpad/b_white/f_auto/q_auto/dpr_auto/c_scale,w_300/v1/ncom/en_US/games/switch/u/unravel-two-switch/hero?_a=AJADJWI0',
+        'https://scontent.fcyw3-1.fna.fbcdn.net/v/t39.30808-6/342062990_907220867001292_6035143614392442733_n.jpg?stp=dst-jpg_p180x540&_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeFDFZ_B03v6MP4oMZXMKXzKjdbvYTim1QKN1u9hOKbVAmP1uJRrofSx0s00dh6DFJyRoQfBgxT2k-FFTOWRniEl&_nc_ohc=kRwzFyhAgxIAb6hvaTk&_nc_ht=scontent.fcyw3-1.fna&oh=00_AfD3PKxmQsawbw9WewJzGCV6Tkx_BdyQUYbhTGdKAvp69g&oe=6628FC5B',
     },
     {
       id: 8,
-      title: 'Unravel Two',
-      category:'Flutter',
+      title: 'Colocacion de alambre de puas',
+      category:'Instalacion',
       rating:4,
-      price: '$59.99',
+      price: 'Colocacion de alambre de puas, con ajuste de malla ciclonica y tornilleria en tensores en casa habitación en calle tecolutla no.111 en colonia bellavista en salamanca,gto.',
       sale: 63,  
       linkImg:
-        'https://assets.nintendo.com/image/upload/ar_16:9,b_auto:border,c_lpad/b_white/f_auto/q_auto/dpr_auto/c_scale,w_300/v1/ncom/en_US/games/switch/u/unravel-two-switch/hero?_a=AJADJWI0',
+        'https://scontent.fcyw3-1.fna.fbcdn.net/v/t39.30808-6/341012045_1436964603740591_6569105352891482580_n.jpg?stp=dst-jpg_s600x600&_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeFpA-L2W5AljUfElq5QrUNwIaF9BzkHO2UhoX0HOQc7ZQ5PIq1Z2yrvoYZU_HhHR9vJjOmMz94sOqSH_8bHvR55&_nc_ohc=xLHrk-h3HzsAb6KPrLK&_nc_ht=scontent.fcyw3-1.fna&oh=00_AfAT8AE9QkoT5_nuVcs_xDIjmBD3aPqhdJOB4zjrr5gP8w&oe=6628CF36',
     },
   ];
