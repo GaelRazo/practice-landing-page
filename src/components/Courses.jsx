72c58282fed890912d0e56f8b3deed9a39cc0513
GaelRazo <57119167+GaelRazo@users.noreply.github.com>
2024-04-19 22:17:08 -0600

    Actu

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -48,8 +48,8 @@ const Courses = () => {
     <div className='w-full bg-[#1788f160] py-32'>
         <div className='md:max-w-[1480px] m-auto max-w-[600px]  px-4 md:px-0'>
             <div className='py-4'>
-              <h1 className='py-3 text-3xl font-bold'>Most Popular <span className='text-[#016AC2]'>Courses</span></h1>
-              <p className='text-[#6D737A]'>Various versions have evolved over the years, sometimes by accident.</p>
+              <h1 className='py-3 text-3xl font-bold'>Algunos de los trabajos de <span className='text-[#016AC2]'>Higuar</span></h1>
+              <p className='text-[#6D737A]'>Recopilacion de trabajos realizados por la empresa.</p>
             </div>
             
             <Slider {...settings} className='px-5'>
